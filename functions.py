cc95b54e7a8a41408fc1fd0bb64bcfc24d5a80fb
ravi-raj1234 <ravifeat.fb@gmail.com>
2024-05-21 21:15:08 +0530

    test

---
+++ functions.py
@@ -0,0 +1,73 @@
+# def my_function(fname):
+#     print(fname + "raj")
+
+# my_function("ravi")
+
+
+
+# def my_function(fname):
+#     print(fname + "raj")
+#     abc = "rahul"
+#     return abc
+    
+# my_function("ravi")
+
+
+# def my_function(x):
+#     return 5 * x
+
+# print (my_function(3))
+
+
+# def my_function(x):
+#     return 5 * x
+# num = int(input("inter the value: "))
+# abc = (my_function(num))
+# print(abc)
+
+
+def my_function(fname):
+    
+    abc = fname
+    return abc
+var = "ravi"
+print(my_function(var))  #insted aaa=(my_function(var))    print(aaa)
+
+
+# def my_function(xyz,abc):
+    
+#     return (xyz+" "+abc)
+# var1 = "ravi"
+# var2 = "raj"
+# rrr = my_function(var1,var2)
+# print(rrr)
+
+
+# def my_function(xyz,abc):
+    
+#     return xyz,abc
+# var1 = "ravi"
+# var2 = "raj"
+# rrr = my_function(var1,var2)
+# print(rrr)
+
+def my_function(xyz,abc):
+    
+    
+  return xyz
+var1 = "ravi"
+var2 = "raj"
+rrr = my_function(var2,var1)
+print(rrr)
+
+# def my_name(abc,xyz):
+#     print(abc+ " " + xyz)
+# my_name("ravi","raj")
+
+
+
+
+
+
+
+
